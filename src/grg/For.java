6f2a4e820c8c9ad967b2e8848f8f39b8d5715b9e
Aurenyx <govindgulhane5@gmail.com>
2026-04-11 23:28:23 +0530

    For Loop

---
+++ src/grg/For.java
@@ -0,0 +1,36 @@
+package grg;//While loop
+//public class While {
+//    public static void main(String[] args) {
+//        int i=100;
+//        while(i<=200){
+//            System.out.println(i);
+//            i++;
+//        }
+//    }
+//}.
+
+//Do while loop
+//public class Do_while{
+//    public static void main(String[] args) {
+//        int i=1;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while(i<10);
+//    }
+//
+//}
+
+// FOR loop
+public class For {
+    public static void main(String[] args) {
+        //2n+1
+//        for(int i=1;i<2000;i++){
+//            System.out.println(2*i+1);
+//        }
+
+        for(int i=5;i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
