536aec332d7ba65af5c403153b151ca42c6600af
Aurenyx <govindgulhane5@gmail.com>
2026-04-11 23:28:59 +0530

    if else statement

---
+++ src/grg/If_Else.java
@@ -0,0 +1,14 @@
+package grg;
+
+public class If_Else {
+    public static void main(String[] args) {
+     int a=7;
+     boolean b= (a==7);
+     if(b) {
+         System.out.println("Can Drive");
+        }
+     else {
+         System.out.println("can't drive");
+     }
+    }
+}
