d433da96baf6cf3fd4bc7666923dc530ffc586b6
Aurenyx <govindgulhane5@gmail.com>
2026-04-11 23:29:43 +0530

    Else If statement

---
+++ src/grg/Else_if.java
@@ -0,0 +1,16 @@
+package grg;
+
+public class Else_if {
+    public static void main(String[] args) {
+        int a=32;
+        if (a>65){
+            System.out.println("can't Work");
+        }
+        else if (a<65){
+            System.out.println("can Work");
+        }
+        else {
+            System.out.println("Now you can't");
+        }
+    }
+}
